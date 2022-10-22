e2d993c881b39d0f787488596bde1cdaeebc09bb
Chris Carrick <christopher.carrick@klaviyo.com>
2022-10-22 00:32:10 -0400

    Remove some checklist comments

--- src/mocks/createMockEvents.ts
+++ src/mocks/createMockEvents.ts
@@ -2,14 +2,6 @@ import { faker } from '@faker-js/faker'
 
 import type { IEvent } from '../types'
 
-// sort by event name then startTime ✅
-// save behavior when navigating to /record if + wasn't clicked ✅
-// only check for updates on app load ✅
-// exporting duplicate filenames ✅
-// delete recordings ✅
-// for each behavior, show total events and total time
-// auto save recording
-
 export function createMockEvents(count = 10): IEvent[] {
   return new Array(count)
     .fill(null)
