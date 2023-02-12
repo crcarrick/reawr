f46078d2c281e7872239a96217fa9ea409540cac
Chris Carrick <chris@crcarrick.dev>
2023-02-12 15:34:07 -0500

    Move issue creation to reawr-api and just call the api from reawr

--- src/api/fileIssue.ts
+++ src/api/fileIssue.ts
@@ -1,35 +1,20 @@
-import type { Octokit } from '@octokit/rest'
-import { ipcMain, ipcRenderer } from 'electron'
-
 import type { IIssue } from '../types'
 
-export function fileIssue({ title, body }: IIssue) {
-  return ipcRenderer.invoke('file:issue', { title, body })
+interface IIssueResponse {
+  readonly url: string
 }
 
-export function handleFileIssue(
-  client: Octokit,
-  callback: (err?: Error) => void
-) {
-  ipcMain.handle(
-    'file:issue',
-    async (_event: Electron.IpcMainEvent, { title, body }: IIssue) => {
-      // TODO: This should really go to some service or backend rather than GH issues
-      //       but GH issues will work for now
-      try {
-        const response = await client.issues.create({
-          owner: 'crcarrick',
-          repo: 'reawr',
-          title,
-          body,
-        })
+export async function fileIssue({
+  title,
+  body,
+}: IIssue): Promise<IIssueResponse> {
+  const response = await fetch('https://reawr-api.vercel.app/api/suggestion', {
+    body: JSON.stringify({ title, body }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
 
-        callback()
-
-        return response
-      } catch (err) {
-        callback(err)
-      }
-    }
-  )
+  return response.json()
 }
--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,7 +1,3 @@
-require('dotenv').config()
-
-const webpack = require('webpack')
-
 const rules = require('./webpack.rules')
 
 module.exports = {
@@ -9,11 +5,6 @@ module.exports = {
   module: {
     rules,
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      GITHUB_ISSUE_TOKEN: JSON.stringify(process.env.GITHUB_ISSUE_TOKEN),
-    }),
-  ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
